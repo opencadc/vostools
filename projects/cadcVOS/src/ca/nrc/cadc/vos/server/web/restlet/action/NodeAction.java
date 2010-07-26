b5b72c07374d3b14c83e454049ccdaed7b5950ea
majorb.cadc <majorb.cadc@728ff76a-78ac-11de-a72b-d90af8dea425>
2010-07-26 17:40:13 +0000

    Javadoc for NodeAction classes
    git-svn-id: https://opencadc.googlecode.com/svn/trunk@744 728ff76a-78ac-11de-a72b-d90af8dea425

--- projects/cadcVOS/src/ca/nrc/cadc/vos/server/web/restlet/action/NodeAction.java
+++ projects/cadcVOS/src/ca/nrc/cadc/vos/server/web/restlet/action/NodeAction.java
@@ -1,3 +1,70 @@
+/**
+ ************************************************************************
+ *******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
+ **************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
+ *
+ *  (c) 2010.                            (c) 2010.
+ *  Government of Canada                 Gouvernement du Canada
+ *  National Research Council            Conseil national de recherches
+ *  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+ *  All rights reserved                  Tous droits réservés
+ *
+ *  NRC disclaims any warranties,        Le CNRC dénie toute garantie
+ *  expressed, implied, or               énoncée, implicite ou légale,
+ *  statutory, of any kind with          de quelque nature que ce
+ *  respect to the software,             soit, concernant le logiciel,
+ *  including without limitation         y compris sans restriction
+ *  any warranty of merchantability      toute garantie de valeur
+ *  or fitness for a particular          marchande ou de pertinence
+ *  purpose. NRC shall not be            pour un usage particulier.
+ *  liable in any event for any          Le CNRC ne pourra en aucun cas
+ *  damages, whether direct or           être tenu responsable de tout
+ *  indirect, special or general,        dommage, direct ou indirect,
+ *  consequential or incidental,         particulier ou général,
+ *  arising from the use of the          accessoire ou fortuit, résultant
+ *  software.  Neither the name          de l'utilisation du logiciel. Ni
+ *  of the National Research             le nom du Conseil National de
+ *  Council of Canada nor the            Recherches du Canada ni les noms
+ *  names of its contributors may        de ses  participants ne peuvent
+ *  be used to endorse or promote        être utilisés pour approuver ou
+ *  products derived from this           promouvoir les produits dérivés
+ *  software without specific prior      de ce logiciel sans autorisation
+ *  written permission.                  préalable et particulière
+ *                                       par écrit.
+ *
+ *  This file is part of the             Ce fichier fait partie du projet
+ *  OpenCADC project.                    OpenCADC.
+ *
+ *  OpenCADC is free software:           OpenCADC est un logiciel libre ;
+ *  you can redistribute it and/or       vous pouvez le redistribuer ou le
+ *  modify it under the terms of         modifier suivant les termes de
+ *  the GNU Affero General Public        la “GNU Affero General Public
+ *  License as published by the          License” telle que publiée
+ *  Free Software Foundation,            par la Free Software Foundation
+ *  either version 3 of the              : soit la version 3 de cette
+ *  License, or (at your option)         licence, soit (à votre gré)
+ *  any later version.                   toute version ultérieure.
+ *
+ *  OpenCADC is distributed in the       OpenCADC est distribué
+ *  hope that it will be useful,         dans l’espoir qu’il vous
+ *  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
+ *  without even the implied             GARANTIE : sans même la garantie
+ *  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
+ *  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
+ *  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
+ *  General Public License for           Générale Publique GNU Affero
+ *  more details.                        pour plus de détails.
+ *
+ *  You should have received             Vous devriez avoir reçu une
+ *  a copy of the GNU Affero             copie de la Licence Générale
+ *  General Public License along         Publique GNU Affero avec
+ *  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
+ *  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
+ *                                       <http://www.gnu.org/licenses/>.
+ *
+ ************************************************************************
+ */
+
 package ca.nrc.cadc.vos.server.web.restlet.action;
 
 import java.io.FileNotFoundException;
@@ -17,6 +84,14 @@ import ca.nrc.cadc.vos.VOSURI;
 import ca.nrc.cadc.vos.server.NodePersistence;
 import ca.nrc.cadc.vos.server.auth.VOSpaceAuthorizer;
 
+/**
+ * Abstract class encapsulating the behaviour of an action on a Node.  Clients
+ * must ensure that setVosURI(), setNodeXML(), setVOSpaceAuthorizer(), and
+ * setNodePersistence() are called before using any concrete implementations of
+ * this class.
+ * 
+ * @author majorb
+ */
 public abstract class NodeAction implements PrivilegedAction<Object>
 {
     protected static Logger log = Logger.getLogger(NodeAction.class);
@@ -26,32 +101,77 @@ public abstract class NodeAction implements PrivilegedAction<Object>
     private VOSURI vosURI;
     private Representation nodeXML;
     
+    /**
+     * Set the URI for this action.
+     * @param vosURI
+     */
     public void setVosURI(VOSURI vosURI)
     {
         this.vosURI = vosURI;
     }
     
+    /**
+     * Set the node XML from the client for this action.
+     * @param nodeXML
+     */
     public void setNodeXML(Representation nodeXML)
     {
         this.nodeXML = nodeXML;
     }
     
+    /**
+     * Set the authorizer to be used by this action.
+     * @param voSpaceAuthorizer
+     */
     public void setVOSpaceAuthorizer(VOSpaceAuthorizer voSpaceAuthorizer)
     {
         this.voSpaceAuthorizer = voSpaceAuthorizer;
     }
     
+    /**
+     * Set the persistence to be used by this action.
+     * @param nodePersistence
+     */
     public void setNodePersistence(NodePersistence nodePersistence)
     {
         this.nodePersistence = nodePersistence;
     }
     
+    /**
+     * Perform the action for which the subclass was designed.
+     * 
+     * The return object from this method (and from performNodeAction) must be an object
+     * of type NodeActionResult.
+     * 
+     * @param node
+     * @param nodePersistence
+     * @return The NodeAction result
+     * @throws Exception If a problem occurs.
+     */
     abstract NodeActionResult performNodeAction(Node node, NodePersistence nodePersistence) throws Exception;
     
+    /**
+     * Given the node URI and XML, return the Node object specified
+     * by the client.
+     */
     abstract Node getClientNode(VOSURI vosURI, Representation nodeXML) throws URISyntaxException, NodeParsingException, IOException;
     
+    /**
+     * Perform an authorization check for the given node and return (if applicable)
+     * the persistent version of the Node.
+     */
     abstract Node doAuthorizationCheck(VOSpaceAuthorizer voSpaceAuthorizer, Node clientNode) throws AccessControlException, FileNotFoundException;
     
+    /**
+     * Entry point in performing the steps of a Node Action.  This includes:
+     * 
+     * Calling abstract method getClientNode()
+     * Calling abstract method doAuthorizationCheck()
+     * Calling abstract method performNodeAction()
+     * 
+     * The return object from this method (and from performNodeAction) must be an object
+     * of type NodeActionResult.
+     */
     @Override
     public Object run()
     {
@@ -115,6 +235,12 @@ public abstract class NodeAction implements PrivilegedAction<Object>
         }
     }
     
+    /**
+     * Create a NodeActionResult from the given fault and message.
+     * @param nodeFault The fault.
+     * @param message An optional message.
+     * @return The new NodeActionResult.
+     */
     private NodeActionResult handleException(NodeFault nodeFault, String message)
     {
         nodeFault.setMessage(message);
