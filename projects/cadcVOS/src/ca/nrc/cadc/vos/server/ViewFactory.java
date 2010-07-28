fc49e609c1fd91c1f9a359ca2364d46f05d0a08c
majorb.cadc <majorb.cadc@728ff76a-78ac-11de-a72b-d90af8dea425>
2010-07-28 22:28:11 +0000

    Addition Views.properties, ViewFactory
    git-svn-id: https://opencadc.googlecode.com/svn/trunk@756 728ff76a-78ac-11de-a72b-d90af8dea425

---
+++ projects/cadcVOS/src/ca/nrc/cadc/vos/server/ViewFactory.java
@@ -0,0 +1,84 @@
+package ca.nrc.cadc.vos.server;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import ca.nrc.cadc.vos.View;
+
+public class ViewFactory
+{
+    
+    private static final String VIEWS_PROPERTY_FILE = "Views";
+    
+    private static final String KEY_VIEW_LIST = "views";
+    private static final String KEY_VIEW_ALIAS = "alias";
+    private static final String KEY_VIEW_URI = "uri";
+    private static final String KEY_VIEW_CLASS = "class";
+    
+    static
+    {
+        loadConfiguredViews();
+    }
+    
+    private static Map<String, View> viewMap;
+    
+    public View getView(String viewReference)
+    {
+        return viewMap.get(viewReference);
+    }
+    
+    private static void loadConfiguredViews()
+    {
+        ResourceBundle rb = null;
+        try
+        {
+            rb = ResourceBundle.getBundle(VIEWS_PROPERTY_FILE);
+        }
+        catch (MissingResourceException e)
+        {
+            throw new ExceptionInInitializerError("Could not load properties file: "
+                    + VIEWS_PROPERTY_FILE + ".properties: " + e.getMessage());
+        }
+        
+        viewMap = new HashMap<String, View>();
+        
+        try
+        {
+            String[] viewNames = rb.getStringArray(KEY_VIEW_LIST);
+            
+            for (String viewName : viewNames)
+            {
+                String viewAlias = rb.getString(viewName + "." + KEY_VIEW_ALIAS);
+                String viewURI = rb.getString(viewName + "." + KEY_VIEW_URI);
+                String viewClassName = rb.getString(viewName + "." + KEY_VIEW_CLASS);
+                Class<?> viewClass = Class.forName(viewClassName);
+                if (!viewClass.isInstance(View.class))
+                {
+                    throw new ExceptionInInitializerError("Class " + viewClass
+                            + " is not an instance of " + View.class);
+                }
+                View view = (View) viewClass.newInstance();
+                if (viewMap.containsKey(viewAlias))
+                {
+                    throw new ExceptionInInitializerError("Duplicate view reference " + viewAlias
+                            + " in file " + VIEWS_PROPERTY_FILE + ".properties");
+                }
+                if (viewMap.containsKey(viewURI))
+                {
+                    throw new ExceptionInInitializerError("Duplicate view reference " + viewURI
+                            + " in file " + VIEWS_PROPERTY_FILE + ".properties");
+                }
+                viewMap.put(viewAlias, view);
+                viewMap.put(viewURI, view);
+            }
+        }
+        catch (Exception e)
+        {
+            throw new ExceptionInInitializerError(e);
+        }
+        
+    }
+
+}
