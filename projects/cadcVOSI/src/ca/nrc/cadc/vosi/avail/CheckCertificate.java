767c2c152dfaa2e3664289ce890547125c9d30ea
pdowler.cadc <pdowler.cadc@728ff76a-78ac-11de-a72b-d90af8dea425>
2010-11-02 17:48:11 +0000

    simple check if an X509 certficate and key exist and are usable (parseable and valid)
    git-svn-id: https://opencadc.googlecode.com/svn/trunk@989 728ff76a-78ac-11de-a72b-d90af8dea425

---
+++ projects/cadcVOSI/src/ca/nrc/cadc/vosi/avail/CheckCertificate.java
@@ -0,0 +1,114 @@
+/*
+************************************************************************
+*******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
+**************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
+*
+*  (c) 2009.                            (c) 2009.
+*  Government of Canada                 Gouvernement du Canada
+*  National Research Council            Conseil national de recherches
+*  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+*  All rights reserved                  Tous droits réservés
+*
+*  NRC disclaims any warranties,        Le CNRC dénie toute garantie
+*  expressed, implied, or               énoncée, implicite ou légale,
+*  statutory, of any kind with          de quelque nature que ce
+*  respect to the software,             soit, concernant le logiciel,
+*  including without limitation         y compris sans restriction
+*  any warranty of merchantability      toute garantie de valeur
+*  or fitness for a particular          marchande ou de pertinence
+*  purpose. NRC shall not be            pour un usage particulier.
+*  liable in any event for any          Le CNRC ne pourra en aucun cas
+*  damages, whether direct or           être tenu responsable de tout
+*  indirect, special or general,        dommage, direct ou indirect,
+*  consequential or incidental,         particulier ou général,
+*  arising from the use of the          accessoire ou fortuit, résultant
+*  software.  Neither the name          de l'utilisation du logiciel. Ni
+*  of the National Research             le nom du Conseil National de
+*  Council of Canada nor the            Recherches du Canada ni les noms
+*  names of its contributors may        de ses  participants ne peuvent
+*  be used to endorse or promote        être utilisés pour approuver ou
+*  products derived from this           promouvoir les produits dérivés
+*  software without specific prior      de ce logiciel sans autorisation
+*  written permission.                  préalable et particulière
+*                                       par écrit.
+*
+*  This file is part of the             Ce fichier fait partie du projet
+*  OpenCADC project.                    OpenCADC.
+*
+*  OpenCADC is free software:           OpenCADC est un logiciel libre ;
+*  you can redistribute it and/or       vous pouvez le redistribuer ou le
+*  modify it under the terms of         modifier suivant les termes de
+*  the GNU Affero General Public        la “GNU Affero General Public
+*  License as published by the          License” telle que publiée
+*  Free Software Foundation,            par la Free Software Foundation
+*  either version 3 of the              : soit la version 3 de cette
+*  License, or (at your option)         licence, soit (à votre gré)
+*  any later version.                   toute version ultérieure.
+*
+*  OpenCADC is distributed in the       OpenCADC est distribué
+*  hope that it will be useful,         dans l’espoir qu’il vous
+*  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
+*  without even the implied             GARANTIE : sans même la garantie
+*  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
+*  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
+*  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
+*  General Public License for           Générale Publique GNU Affero
+*  more details.                        pour plus de détails.
+*
+*  You should have received             Vous devriez avoir reçu une
+*  a copy of the GNU Affero             copie de la Licence Générale
+*  General Public License along         Publique GNU Affero avec
+*  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
+*  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
+*                                       <http://www.gnu.org/licenses/>.
+*
+*  $Revision: 4 $
+*
+************************************************************************
+*/
+
+package ca.nrc.cadc.vosi.avail;
+
+import ca.nrc.cadc.auth.SSLUtil;
+import java.io.File;
+import javax.security.auth.Subject;
+
+/**
+ * @author zhangsa
+ *
+ */
+public class CheckCertificate implements CheckResource
+{
+    private File cert;
+    private File key;
+
+    /**
+     * Constructor to check a DataSource.
+     * The test query should be something that always executes quickly and
+     * returns a small result set, such as <code>select x from someTable limit 0</code>.
+     *
+     * @param dataSource the DataSource to check
+     * @param testSQL test quiery that should work
+     */
+    public CheckCertificate(File cert, File key)
+    {
+        this.cert = cert;
+        this.key = key;
+    }
+
+    @Override
+    public void check()
+        throws CheckException
+    {
+        try
+        {
+            Subject s = SSLUtil.createSubject(cert, key);
+            // OK: certficate exists and is valid
+
+        }
+        catch(Throwable t)
+        {
+            throw new CheckException("certificate check failed", t);
+        }
+    }
+}
