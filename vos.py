9007362e16c5a789123a668afcdb0b8d59cef9c6
JJ Kavelaars <JJ.Kavelaars@nrc.gc.ca>
2011-10-18 19:17:14 -0700

    corrected the comments in the header

--- vos.py
+++ vos.py
@@ -1,11 +1,5 @@
 """A set of Python Classes for connecting to and interacting with a VOSpace service.
 
-   list     : provides a the contents of a VOSpace ContainerNode or information about a DataNode
-   create   : new VOSpace ContainerNode
-   delete   : removes a VOSpace [Container|Data]Node
-   retrieve : pull a VOSpace DataNode
-   push     : push a file to a VOSpace DataNode
-
    Connections to VOSpace are made using a SSL X509 certificat which is stored in a .pem file.  
    The certificate is supplied by the user or by the CADC credential server
 
