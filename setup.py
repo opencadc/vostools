87e781e74861a5bf67a4709a6db1baeb44ac4a03
JJ Kavelaars <JJ.Kavelaars@nrc.gc.ca>
2011-10-28 08:12:35 -0700

    Added install of new programs to the setup routines

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(name="vofs",
       author="JJ Kavelaars",
       author_email="jj.kavelaars@nrc.gc.ca",
       py_modules=['fuse','vos'],
-      scripts=['getCert.py','vcp','vrm','vls','vmkdir','mountvofs']
+      scripts=['getCert.py','vcp','vrm','vls','vmkdir','mountvofs','vrmdir']
 )
