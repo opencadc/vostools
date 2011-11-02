81e9c69357de5fa1142336323e413468b2ca708f
adrian.damian@nrc.ca <adrian.damian@nrc.ca@728ff76a-78ac-11de-a72b-d90af8dea425>
2011-11-02 03:46:50 +0000

    Revert to previous version
    git-svn-id: https://opencadc.googlecode.com/svn/trunk@1407 728ff76a-78ac-11de-a72b-d90af8dea425

--- projects/cadcVOFS/scripts/vos.py
+++ projects/cadcVOFS/scripts/vos.py
@@ -491,7 +491,6 @@ class VOFile:
         self.httpCon = self.connector.getConnection(URL)
         if self.timeout < 0 : 
 	    self.timeout=time.time()
-	    import httplib
         try:
             self.httpCon.connect()
         #except ssl.SSLError as e:
