bb33df4b8d1747bf3e22709576724ea55454eb04
JJ Kavelaars <jj.kavelaars@nrc-cnrc.gc.ca>
2012-08-21 18:13:46 -0700

    cleaned up the threading and locking for sqlite db that has the md5 values

--- vos/md5_cache.py
+++ vos/md5_cache.py
@@ -10,55 +10,38 @@ class MD5_Cache:
 
       ## initialize the md5Cache db
       sqlConn = sqlite3.connect(self.cache_db)
-      sqlConn.execute("create table if not exists md5_cache (fname text, md5 text, st_mtime int)")
-      sqlConn.commit()
+      with sqlConn:
+         sqlConn.execute("create table if not exists md5_cache (fname text PRIMARY KEY NOT NULL , md5 text, st_mtime int)")
       sqlConn.close()
       ## build cache lookup if doesn't already exists
 
    def get(self,fname):
       """Get the MD5 for this fname from the SQL cache"""
       sqlConn=sqlite3.connect(self.cache_db)
-      sqlConn.row_factory = sqlite3.Row
-      cursor= sqlConn.cursor()
-      cursor.execute("SELECT * FROM md5_cache WHERE fname = ?", (fname,))
-      md5Row=cursor.fetchone()
-      cursor.close()
+      with sqlConn:
+          cursor=sqlConn.execute("SELECT md5,st_mtime FROM md5_cache WHERE fname = ?", (fname,))
+          md5Row=cursor.fetchone()
       sqlConn.close()
-      return md5Row
+      if md5Row is not None: 
+	return md5Row[0],md5Row[1]
+      else:	
+        return None
    
    def delete(self,fname):
       """Delete a record from the cache MD5 database"""
       sqlConn=sqlite3.connect(self.cache_db)
-      sqlConn.row_factory =  sqlite3.Row
-      cursor = sqlConn.cursor()
-      cursor.execute("DELETE from md5_cache WHERE fname = ?", ( fname,))
-      sqlConn.commit()
-      cursor.close()
+      with sqlConn:
+           sqlConn.execute("DELETE from md5_cache WHERE fname = ?", ( fname,))
       sqlConn.close()
-      return
    
    
-   def update(self,fname):
+   def update(self,fname,md5):
       """Update a record in the cache MD5 database"""
-      import hashlib, os
-      md5=hashlib.md5()
-      r=open(fname,'r')
-      while True:
-         buf=r.read(READBUF)
-         if len(buf)==0:
-            break
-         md5.update(buf)
-      r.close()
-         
+      import os 
       ## UPDATE the MD5 database
       sqlConn=sqlite3.connect(self.cache_db)
-      sqlConn.row_factory = sqlite3.Row
-      cursor=sqlConn.cursor()
-      cursor.execute("DELETE FROM md5_cache WHERE fname = ?", (fname,))
-      if md5 is not None:
-         cursor.execute("INSERT INTO md5_cache (fname, md5, st_mtime) VALUES ( ?, ?, ?)", (fname, md5.hexdigest(), os.stat(fname).st_mtime))
-      sqlConn.commit()
-      cursor.close()
+      with sqlConn:
+         sqlConn.execute("DELETE from md5_cache WHERE fname = ?", ( fname,))
+         sqlConn.execute("INSERT INTO md5_cache (fname, md5, st_mtime) VALUES ( ?, ?, ?)", (fname, md5, os.stat(fname).st_mtime ))
       sqlConn.close()
-      return
 
