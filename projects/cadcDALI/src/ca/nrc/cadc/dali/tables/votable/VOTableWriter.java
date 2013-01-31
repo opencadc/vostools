a745dbf81d80096ce72e1bb6477dd4f6587776e8
pdowler.cadc@gmail.com <pdowler.cadc@gmail.com@728ff76a-78ac-11de-a72b-d90af8dea425>
2013-01-31 23:40:26 +0000

    fixed votable mimetype
    git-svn-id: https://opencadc.googlecode.com/svn/trunk@1873 728ff76a-78ac-11de-a72b-d90af8dea425

--- projects/cadcDALI/src/ca/nrc/cadc/dali/tables/votable/VOTableWriter.java
+++ projects/cadcDALI/src/ca/nrc/cadc/dali/tables/votable/VOTableWriter.java
@@ -93,7 +93,7 @@ public class VOTableWriter implements TableWriter<VOTable>
 {
     private static final Logger log = Logger.getLogger(VOTableWriter.class);
 
-    public static final String CONTENT_TYPE = "text/x-votable+xml";
+    public static final String CONTENT_TYPE = "application/x-votable+xml";
     
     // VOTable Version number.
     public static final String VOTABLE_VERSION  = "1.2";
