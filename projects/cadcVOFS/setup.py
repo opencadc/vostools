714e9afdad6839580555f4ee5c3d8181283cb8be
jjkavelaars@gmail.com <jjkavelaars@gmail.com@728ff76a-78ac-11de-a72b-d90af8dea425>
2013-03-20 05:07:06 +0000

    cleaned up some cruft and made decription more acurate
    git-svn-id: https://opencadc.googlecode.com/svn/trunk@1928 728ff76a-78ac-11de-a72b-d90af8dea425

--- projects/cadcVOFS/setup.py
+++ projects/cadcVOFS/setup.py
@@ -1,41 +1,3 @@
-#/*+
-#************************************************************************
-#****  C A N A D I A N   A S T R O N O M Y   D A T A   C E N T R E  *****
-#*
-#* (c) 2012.                            (c)2012.
-#* National Research Council            Conseil national de recherches
-#* Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
-#* All rights reserved                  Tous droits reserves
-#*
-#* NRC disclaims any warranties,        Le CNRC denie toute garantie
-#* expressed, implied, or statu-        enoncee, implicite ou legale,
-#* tory, of any kind with respect       de quelque nature que se soit,
-#* to the software, including           concernant le logiciel, y com-
-#* without limitation any war-          pris sans restriction toute
-#* ranty of merchantability or          garantie de valeur marchande
-#* fitness for a particular pur-        ou de pertinence pour un usage
-#* pose.  NRC shall not be liable       particulier.  Le CNRC ne
-#* in any event for any damages,        pourra en aucun cas etre tenu
-#* whether direct or indirect,          responsable de tout dommage,
-#* special or general, consequen-       direct ou indirect, particul-
-#* tial or incidental, arising          ier ou general, accessoire ou
-#* from the use of the software.        fortuit, resultant de l'utili-
-#*                                      sation du logiciel.
-#*
-#************************************************************************
-#*
-#*   Script Name:       setup.py
-#*
-#*   Purpose:
-#*      Distutils setup script for caom2
-#*
-#*   Functions:
-#*
-#*
-#*
-#****  C A N A D I A N   A S T R O N O M Y   D A T A   C E N T R E  *****
-#************************************************************************
-#-*/
 
 # Use "distribute"
 from setuptools import setup, find_packages
@@ -46,25 +8,11 @@ if sys.version_info[0] > 2:
     sys.exit(-1)
 
 
-#from distutils.core import Command
-
-
-#class PyTest(Command):
-#    user_options = []
-#    def initialize_options(self):
-#        pass
-#    def finalize_options(self):
-#        pass
-#    def run(self):
-#        import sys,subprocess
-#        errno = subprocess.call([sys.executable, 'runtests.py'])
-#        raise SystemExit(errno)
-
 from vos.__version__ import version
 setup(name="vos",
       version=version,
       url="https://github.com/ijiraq/cadcVOFS",
-      description="CADC VOSpace Filesystem",
+      description="Tools for interacting with CADC VOSpace.",
       author="JJ Kavelaars",
       author_email="jj.kavelaars@nrc.gc.ca",
       packages=['vos'],
@@ -80,5 +28,4 @@ setup(name="vos",
         'Operating System :: POSIX',
         'Programming Language :: Python',
         ],    
-#      cmdclass = {'test': PyTest}
 )
