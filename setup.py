2bd25a106ebd9d0b412f0c02b9a0b11090e2f57b
JJ Kavelaars <jjkavelaars@gmail.com>
2013-03-19 22:12:06 -0700

    moved scripts to scripts directory

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
+
 # Use "distribute"
 from setuptools import setup, find_packages
 import sys
 
+if sys.version_info[0] > 2:
+    print 'The caom2 package is only compatible with Python version 2.n'
+    sys.exit(-1)
+
 
 from vos.__version__ import version
-
 setup(name="vos",
       version=version,
       url="https://github.com/ijiraq/cadcVOFS",
@@ -13,7 +17,7 @@ setup(name="vos",
       author_email="jj.kavelaars@nrc.gc.ca",
       packages=['vos'],
       install_requires=['distribute'],
-      scripts=['getCert','vsync','vmv','vcp','vrm','vls','vmkdir','mountvofs','vrmdir', 'vln', 'vcat', 'vtag', 'vchmod', 'checkJobPhase' ],
+      scripts=['scripts/getCert','scripts/vsync','scripts/vmv','scripts/vcp','scripts/vrm','scripts/vls','scripts/vmkdir','scripts/mountvofs','scripts/vrmdir', 'scripts/vln', 'scripts/vcat', 'scripts/vtag', 'scripts/vchmod', 'scripts/checkJobPhase' ],
       classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
