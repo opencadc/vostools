c9fd1897458c81207dc0f0a8ef054090a8346852
drusk <drusk@uvic.ca>
2013-05-02 11:51:19 -0700

    Added basic test case for Client.getNodeURL with data view.

--- test/test_vos/test_client.py
+++ test/test_vos/test_client.py
@@ -10,12 +10,25 @@ TEST_URI = "vos://naming_authority/mydata/file1"
 
 class ClientTest(unittest.TestCase):
     def setUp(self):
-        self.conn = Mock()
+        self.conn = Mock(spec=vos.Connection)
+
         self.undertest = vos.Client(conn=self.conn)
 
     def test_open_basic_vofile(self):
         vofile = self.undertest.open(TEST_URI)
         assert_that(vofile.name, equal_to("file1"))
+
+    def test_getNodeURL_viewdata(self):
+        test_endpoint = "http://www.testendpoint.ca"
+        self.undertest.transfer = Mock(return_value=test_endpoint)
+
+        url = self.undertest.getNodeURL(TEST_URI, view="data")
+        assert_that(url, equal_to(test_endpoint))
+        self.undertest.transfer.assert_called_with(TEST_URI, "pullFromVoSpace")
+
+        url = self.undertest.getNodeURL(TEST_URI, view="data", method="PUT")
+        assert_that(url, equal_to(test_endpoint))
+        self.undertest.transfer.assert_called_with(TEST_URI, "pushToVoSpace")
 
 
 if __name__ == '__main__':
