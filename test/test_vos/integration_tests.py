bf15256903661e67052c94fa9cb9b4d0860bb767
drusk <drusk@uvic.ca>
2013-05-03 11:33:25 -0700

    Added integration test for opening a cutout of a file in vospace.

--- test/test_vos/integration_tests.py
+++ test/test_vos/integration_tests.py
@@ -28,6 +28,22 @@ class IntegrationTest(unittest.TestCase):
         assert_that(hdulist[0].data.shape, equal_to((4, 200, 200)))
         assert_that(hdulist[1].data.shape, equal_to((4,)))
 
+    def test_open_cutout(self):
+        client = vos.Client()
+        uri = "vos://cadc.nrc.ca~vospace/drusk/testdata.fits"
+        vofile = client.open(uri, view="cutout", cutout="[0][80:140,50:120]")
+
+        rawdata = cStringIO.StringIO(vofile.read())
+        hdulist = fits.open(rawdata)
+
+        assert_that(hdulist, has_length(1))
+
+        assert_that(hdulist[0].header["FILENAME"],
+                    equal_to("u5780205r_cvt.c0h"))
+
+        # NOTE: Pixel range is inclusive
+        assert_that(hdulist[0].data.shape, equal_to((4, 71, 61)))
+
 
 if __name__ == '__main__':
     unittest.main()
