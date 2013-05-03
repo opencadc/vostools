f6b5f12d3217955a3ca34cc0be4bf66eebb83b93
drusk <drusk@uvic.ca>
2013-05-03 10:17:30 -0700

    Added integration test for loading fits file from vospace.

---
+++ test/test_vos/integration_tests.py
@@ -0,0 +1,33 @@
+"""
+Integration level tests.  Requires internet connection to access
+real vospace data.
+"""
+import unittest
+import cStringIO
+
+from astropy.io import fits
+from hamcrest import assert_that, equal_to, has_length
+
+import vos
+
+
+class IntegrationTest(unittest.TestCase):
+    def test_open(self):
+        client = vos.Client()
+        uri = "vos://cadc.nrc.ca~vospace/drusk/testdata.fits"
+        vofile = client.open(uri, view="data")
+
+        rawdata = cStringIO.StringIO(vofile.read())
+        hdulist = fits.open(rawdata)
+
+        assert_that(hdulist, has_length(2))
+
+        assert_that(hdulist[0].header["FILENAME"],
+                    equal_to("u5780205r_cvt.c0h"))
+
+        assert_that(hdulist[0].data.shape, equal_to((4, 200, 200)))
+        assert_that(hdulist[1].data.shape, equal_to((4,)))
+
+
+if __name__ == '__main__':
+    unittest.main()
