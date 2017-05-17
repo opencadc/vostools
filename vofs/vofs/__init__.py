59cae2455cdfbb35af00b249acc461228d308c34
Adrian Damian <Adrian.Damian@nrc.ca>
2017-05-17 14:55:57 -0700

    Removed dependency on astropy and numpy

--- vofs/vofs/__init__.py
+++ vofs/vofs/__init__.py
@@ -1,16 +1,11 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 """
-This is an Astropy affiliated package.
+This application mounts a VOSpace service to the local file system
+ using fuse.
 """
-
-# Affiliated packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._astropy_init import *
-# ----------------------------------------------------------------------------
-
-# For egg_info test builds to pass, put package imports here.
-if not _ASTROPY_SETUP_:
-    #from .example_mod import *
+try:
+    _PACKAGE_SETUP_
+except NameError:
+    # not in setup mode
     from vofs import *
