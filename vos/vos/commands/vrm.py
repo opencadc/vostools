a9112264b22da99f2e968d10e899948befac8dc8
Alinga Yeung <Alinga.Yeung@nrc-cnrc.gc.ca>
2020-01-31 15:49:16 -0800

    Story 2614. Updated vrm to support storage.

--- vos/vos/commands/vrm.py
+++ vos/vos/commands/vrm.py
@@ -11,6 +11,38 @@ node or node is locked.
 eg. vrm vos:/root/node   -- deletes a data node"""
 
 
+def delete_nodes(args):
+    client = vos.Client(vospace_certfile=args.certfile,
+                        vospace_token=args.token)
+    for node in args.source:
+        if not node.startswith('vos:'):
+            raise Exception(
+                '{} is not a valid VOSpace handle'.format(node))
+        if not node.endswith('/'):
+            if client.get_node(node).islink():
+                logging.info('deleting link {}'.format(node))
+                client.delete(node)
+            elif client.isfile(node):
+                logging.info('deleting {}'.format(node))
+                client.delete(node)
+        elif client.isdir(node):
+            raise Exception('{} is a directory'.format(node))
+        else:
+            raise Exception('{} is not a directory'.format(node))
+
+
+def delete_files(args):
+    client = vos.StorageClient(storage_certfile=args.certfile,
+                               storage_token=args.token)
+    for file in args.source:
+        if not file.startswith('vos:'):
+            raise Exception(
+                '{} is not a valid storage handle'.format(file))
+        else:
+            logging.info('deleting {}'.format(file))
+            client.delete(file)
+
+
 def vrm():
     parser = CommonParser(description=DESCRIPTION)
     parser.add_argument(
@@ -25,23 +57,10 @@ def vrm():
     set_logging_level_from_args(args)
 
     try:
-        client = vos.Client(vospace_certfile=args.certfile,
-                            vospace_token=args.token)
-        for node in args.node:
-            if not node.startswith('vos:'):
-                raise Exception(
-                    '{} is not a valid VOSpace handle'.format(node))
-            if not node.endswith('/'):
-                if client.get_node(node).islink():
-                    logging.info('deleting link {}'.format(node))
-                    client.delete(node)
-                elif client.isfile(node):
-                    logging.info('deleting {}'.format(node))
-                    client.delete(node)
-            elif client.isdir(node):
-                raise Exception('{} is a directory'.format(node))
-            else:
-                raise Exception('{} is not a directory'.format(node))
+        if args.resourceID is None:
+            delete_nodes(args)
+        else:
+            delete_files(args)
 
     except Exception as ex:
         exit_on_exception(ex)
